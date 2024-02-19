965301af69f628a8e232ca99afa8f145ef19cba0
Nethajiparthinan <Nethajiparthiban@gmail.com>
2024-02-19 18:56:26 +0530

    project completed with 90% Accuracy

--- project1.py
+++ project1.py
@@ -23,3 +23,26 @@ from sklearn.preprocessing import LabelEncoder
 df[['Buying','meant','doors','persons','lug_boot',
     'safety','class']]=df[['Buying','meant','doors','persons','lug_boot',
     'safety','class']].apply(LabelEncoder().fit_transform)
+#print(df.head())
+X=df.iloc[:,:-1]
+Y=df.iloc[:,-1]
+#print(X.head())
+#Training the model
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test=train_test_split(X,Y,test_size=0.25,random_state=1)
+#fitting to Decision Tree model.
+from sklearn.tree import DecisionTreeRegressor
+tree=DecisionTreeRegressor()
+tree.fit(x_train,y_train)
+y_pred=tree.predict(x_test)
+#print(tree.score(x_test,y_test)*100)
+import matplotlib.pyplot as plt
+# plt.scatter(y_pred,y_test)
+# plt.plot(y_test,y_pred)
+# plt.title('Car values prediction')
+# plt.xlabel('x values')
+# plt.ylabel('y values')
+# plt.show()
+plt.scatter(y_test,y_pred,color='red')
+plt.plot(y_test,y_pred,color='blue')
+plt.show()
